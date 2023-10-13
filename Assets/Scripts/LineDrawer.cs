9449c9120a5f6c5541b5f516554327bd184cf4b9
Студент <student@ad.anosov.ru>
2023-10-13 13:21:54 +0500

    Изменена модель для остальных блок схем, всех их можно соеденить друг с другом

--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using Unity.VisualScripting;
 
 public class LineDrawer : MonoBehaviour
 {
+
     private LineRenderer _lineRend;
     private Vector2 _mousePos;
     private Vector2 _startMousePos;
@@ -15,13 +19,13 @@ public class LineDrawer : MonoBehaviour
 
     private bool canDrawLine = false;
     private bool isConnected = false;
+
     // Start is called before the first frame update
     void Start()
     {
         _lineRend = GetComponent<LineRenderer>();
         _lineRend.positionCount = 2;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -29,7 +33,6 @@ public class LineDrawer : MonoBehaviour
         if (Input.GetMouseButton(0) && canDrawLine && !isConnected)
         {
             _startMousePos = startPos.transform.position;
-
             _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             _lineRend.SetPosition(0, new Vector2(_startMousePos.x, _startMousePos.y));
             _lineRend.SetPosition(1, new Vector2(_mousePos.x, _mousePos.y));
